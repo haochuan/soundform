7126ac34faac42c5e8f09d180458bdc3d842a155
Haochuan Liu <haochuan.liu@gmail.com>
2015-11-18 16:56:28 -0800

    add audioContext store

--- src/AudioContext/stores/AudioContextStore.js
+++ src/AudioContext/stores/AudioContextStore.js
@@ -1,8 +1,18 @@
 import { Store, toImmutable } from 'nuclear-js';
+import randomString from 'randomstring';
 
 export default Store({
     getInitialState() {
-        return toImmutable({audioContext: this.initAudioContext()})
+        return toImmutable({
+            audioContext: this.initAudioContext(),
+            audioNodes: {}
+        });
+    },
+
+    initialize() {
+        this.on(ADD_NODE, addNode);
+        this.on(UPDATE_NODE, updateNode);
+        this.on(DELETE_NODE, deleteNode);
     },
 
     initAudioContext() {
@@ -15,4 +25,20 @@ export default Store({
         }
         return audioContext;
     }
-})
+});
+
+/**
+
+    TODO:
+    - Seems imposible to write the audio node into an immutable data structure
+
+ */
+
+function addNode(state, audioNode) {
+    let id = randomstring.generate(16);
+    return state.setIn(['audioNodes', id], audioNode);
+}
+
+function updateNode(state, id) {
+    return state.updateIn(['audioNodes', id], id);
+}
